7615c89671a844c65339c9afd92da5667c562413
mojmax <mojettam@gmail.com>
2017-03-17 16:52:41 +0100

    Final Commit

---
+++ src/main/java/it/moj/webapp/controller/SearchController.java
@@ -0,0 +1,85 @@
+package it.moj.webapp.controller;
+
+import java.util.List;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Image;
+import org.zkoss.zul.Label;
+import org.zkoss.zul.ListModelList;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Textbox;
+
+import it.moj.webbapp.model.Car;
+import it.moj.webbapp.model.CarService;
+import it.moj.webbapp.model.CarServiceImpl;
+
+public class SearchController extends SelectorComposer<Component> {
+	@Wire
+	private Textbox keywordBox;
+	
+	@Wire
+	private Listbox carListbox;
+	
+	@Wire
+	private Label keywordBoxSearched;
+	
+	@Wire 
+	private Image previewImage;
+	
+	@Wire
+	private Label modelLabel;
+	
+	@Wire
+	private Label makeLabel;
+	
+	@Wire
+	private Label priceLabel;
+	
+	@Wire 
+	private Label descriptionLabel;
+
+	
+	CarService carService = new CarServiceImpl();
+		
+	@Listen("onClick = button#searchButton")
+	public void search() {
+		String key = keywordBox.getValue();
+		System.out.println("key " + key);	
+		List<Car> result = carService.search(key);
+		carListbox.setModel(new ListModelList<Car>(result));
+		if ( result.isEmpty() ) {
+			keywordBoxSearched.setVisible(false);
+		}
+		else {
+			keywordBoxSearched.setVisible(true);
+			if (key.equals("") ) { 
+				keywordBoxSearched.setValue("Founded :  All row Returned");
+			} else {
+				keywordBoxSearched.setValue("Founded : " + key);
+			}
+			
+			if ( carListbox.getItemCount() == 1 ) {
+				carListbox.set;
+			}
+			//showDetail();
+		}
+	}
+	
+	@Listen("onSelect = #carListbox")
+	public void showDetail() {
+		//???
+		System.out.println("showDetail()");
+		Car selected = carListbox.getSelectedItem().getValue();
+		//previewImage.setSrc(selected.getImagePath());
+		previewImage.setSrc(selected.getImagePath());
+		System.out.println(selected.getImagePath());
+		modelLabel.setValue(selected.getModel());
+		makeLabel.setValue(selected.getMake());
+		priceLabel.setValue(selected.getPrice().toString());
+		descriptionLabel.setValue(selected.getPreview());
+	}
+	
+}
---
+++ src/main/java/it/moj/webbapp/model/Car.java
@@ -0,0 +1,94 @@
+package it.moj.webbapp.model;
+
+
+public class Car {
+
+	private Integer id;
+	private String model;
+	private String make;
+	private String preview;
+	private String imagePath;
+	private Integer price;
+	
+	public Car(){
+	}
+	
+	public Car(Integer id, String model, String make, String preview,  String imagePath, Integer price){
+		this.id = id;
+		this.model = model;
+		this.make = make;
+		this.preview = preview;
+		this.imagePath = imagePath;
+		this.price = price;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getPreview() {
+		return preview;
+	}
+	public void setPreview(String preview) {
+		this.preview = preview;
+	}
+	
+	public String getImagePath() {
+		return imagePath;
+	}
+	
+	public void setImagePath(String image) {
+		this.imagePath = imagePath;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+/*	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}*/
+
+/*	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Car))
+			return false;
+		Car other = (Car) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}*/
+
+}
