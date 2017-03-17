1a4cf974e029d48127bde8d74553e42655ba083d
mojmax <mojettam@gmail.com>
2017-03-17 11:35:02 +0100

    First

---
+++ src/main/java/it/moj/webapp/MyViewModel.java
@@ -0,0 +1,26 @@
+package it.moj.webapp;
+
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
+
+public class MyViewModel {
+
+	private int count;
+
+	@Init
+	public void init() {
+		count = 100;
+	}
+
+	@Command
+	@NotifyChange("count")
+	public void cmd() {
+		++count;
+		++count;
+	}
+
+	public int getCount() {
+		return count;
+	}
+}
